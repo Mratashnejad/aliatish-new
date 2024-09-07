cba3621e6debe39294bad8ac885f83e3dbe43443
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-09-07 11:16:13 +0400

    updates

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { FaGithub, FaStackOverflow, FaKaggle } from 'react-icons/fa';
 import { HiDownload } from 'react-icons/hi';
+import FAQPage from '@/components/faq';
 
 export default function AboutPage() {
     return (
-        <section
+        <>
+        {/* About Us Section */}
+        <div
             id="about"
             className="w-full min-h-screen py-28 px-6 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-400 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700"
         >
             <div className="container mx-auto text-center">
                 <h2 className="text-5xl font-bold text-gray-900 dark:text-gray-100 mb-12">About Us</h2>
-
+        
                 {/* Social Media Links */}
                 <div className="mb-12">
                     <h3 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-6">Connect with Us</h3>
@@ -55,7 +58,7 @@ export default function AboutPage() {
                             <li>Deliver 100 personal websites, each tailored to the unique needs of individuals and businesses.</li>
                             <li>Create 10 online shops that drive e-commerce success through innovative design and functionality.</li>
                         </ul>
-                        I am committed to advancing my skills and achieving these goals, continually pushing the boundaries of what&aposs possible in web development.
+                        I am committed to advancing my skills and achieving these goals, continually pushing the boundaries of what's possible in web development.
                     </p>
                     <h3 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 mb-6">Our Values</h3>
                     <ul className="list-disc list-inside text-gray-700 dark:text-gray-300 mb-6">
@@ -77,6 +80,12 @@ export default function AboutPage() {
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+
+        {/* FAQ Section */}
+        <div className="py-16 bg-white dark:bg-gray-800">
+            <FAQPage />
+        </div>
+        </>
     );
 }

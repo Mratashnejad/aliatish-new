eb0a075cbb30f22c072eb437cb35e396be294bc7
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-10-27 19:58:01 +0400

    UPDATE FOR 2 CORE

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    workerThreads: false,
+    cpus: 2, // Matches your 2-core CPU
+  },
+};
 
 export default withNextIntl(nextConfig);
-
-

b49f15789e448bb54ad9fa4eaeedb0f1f76eb163
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-11-11 13:20:44 +0400

    update

--- lib/auth/checkIsAuthenticated.ts
+++ lib/auth/checkIsAuthenticated.ts
@@ -1,11 +1,11 @@
-"use server";
+'use server';
 
-import { auth } from "@/lib/auth/authConfig";
+import { auth } from '@/lib/auth/authConfig';
 
 export const checkIsAuthenticated = async () => {
-  const session = await auth();
-  if (session) {
-    return true;
-  }
-  return false;
+    const session = await auth();
+    if (session) {
+        return true;
+    }
+    return false;
 };

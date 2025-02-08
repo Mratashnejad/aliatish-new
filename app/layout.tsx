b3bfb8a42949a0b96c48caae8ba4c847ab155d49
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2025-02-08 10:02:18 +0400

    add to secound layout GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,21 +5,21 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
     title: {
         default: 'Website Development & Design Services | Modern Web Solutions',
-        template: '%s | AliAtish', // This ensures each page's title will follow the default format with page-specific titles
+        template: '%s | AliAtish',
     },
     description:
         'Ali Atish, Web Application design, offers website development and design services specializing in scalable web applications using modern technologies like Next.js, TypeScript, Node.js, and PostgreSQL.',
-    keywords: 'Website Development, Full Stack Developer, Web Design Services,Nest.js Next.js, TypeScript, PostgreSQL, Node.js, scalable web applications, custom web development, modern technologies, UI/UX design, AI/ML, professional web solutions',
+    keywords: 'Website Development, Full Stack Developer, Web Design Services, Nest.js, Next.js, TypeScript, PostgreSQL, Node.js, scalable web applications, custom web development, modern technologies, UI/UX design, AI/ML, professional web solutions',
     twitter: {
-        card: 'summary_large_image', // Optimized for sharing on Twitter with rich media
+        card: 'summary_large_image',
     },
     openGraph: {
-        title: 'Website Development & Design Services | Modern Web Solutions', // Open Graph metadata for social sharing
+        title: 'Website Development & Design Services | Modern Web Solutions',
         description:
             'Explore professional web development services by Ali Atish. We specialize in building scalable, high-performance web applications using the latest technologies like Next.js, TypeScript, PostgreSQL, and more.',
-        url: 'https://aliatish.com', // Add your site URL to make the metadata complete for social platforms
+        url: 'https://aliatish.com',
         siteName: 'Ali Atish | Web Development & Design',
-        type: 'website', // Indicating that this is a website for social media links
+        type: 'website',
     },
 };
 
@@ -31,8 +31,32 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                {/* Google Tag Manager - Head */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-MP34XP5N');
+                        `,
+                    }}
+                />
             </head>
-            <body>{children}</body>
+            <body>
+                {/* Google Tag Manager - Body */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-MP34XP5N"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
+
+                {children}
+            </body>
         </html>
     );
 }
